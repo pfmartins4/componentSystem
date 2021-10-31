b8fa238cae923c2f84497a82ce9a45b57ec1e95c
Pedro Martins <pfmartins4@gmail.com>
2021-10-31 13:40:48 -0300

    creating form hooks

--- src/components/Form/hooks/formReducer.js
+++ src/components/Form/hooks/formReducer.js
@@ -1,4 +1,14 @@
-const formReducer = (state, { type, payload }) => {
+export const initialization = ({ values, errors }) => ({
+  values,
+  errors,
+  isSubmiting: false,
+  isValidating: false,
+  submitAttempt: 0,
+  submitSuccess: 0,
+  submitFailure: 0,
+});
+
+export const formReducer = (state, { type, payload }) => {
   switch (type) {
     case "SET_VALUES":
       return { ...state, values: payload };
@@ -7,21 +17,32 @@ const formReducer = (state, { type, payload }) => {
     case "SET_ERRORS":
       return { ...state, errors: payload };
     case "SET_FIELD_VALUE":
-      return; /** @todo implementar */
+      return {
+        ...state,
+        values: { ...state.values, [payload.name]: payload.value },
+      };
     case "SET_FIELD_TOUCHED":
-      return; /** @todo implementar */
+      return {
+        ...state,
+        touched: { ...state.touched, [payload.name]: payload.touched },
+      };
     case "SET_FIELD_ERROR":
-      return; /** @todo implementar */
+      return {
+        ...state,
+        errors: { ...state.errors, [payload.name]: payload.error },
+      };
     case "SET_ISVALIDATING":
-      return; /** @todo implementar */
+      return { ...state, isValidating: { isValidating, payload } };
     case "SET_ISSUBMITING":
-      return; /** @todo implementar */
-    case "SUBMIT_ATTEMPT":
-      return; /** @todo implementar */
-    case "SUBMIT_SUCCESS":
-      return; /** @todo implementar */
-    case "SUBMIT_FAILURE":
-      return; /** @todo implementar */
+      return { ...state, isSubmiting: payload };
+    case "SET_SUBMIT_ATTEMPT":
+      return { ...state, submitAttempt: payload };
+    case "SET_SUBMIT_SUCCESS":
+      return { ...state, submitSuccess: payload };
+    case "SET_SUBMIT_FAILURE":
+      return { ...state, submitFailure: payload };
+    case "RESET":
+      return initialization(payload);
     default:
       return state;
   }
--- src/components/Form/hooks/useFormContext.js
+++ src/components/Form/hooks/useFormContext.js
@@ -1,14 +1,41 @@
 import { createContext, useContext, useEffect, useState } from "react";
+import { formReducer, initialization } from "./formReducer";
 
 const FormContext = createContext();
 
 FormContext.displayName = "FormContext";
-const FormProvider = FormContext.Provider;
-const FormConsumer = FormContext.Consumer;
-const useFormContext = ({ fields, onReset, onSubmit }) => {
-  const form = useContext(form);
+
+const 
+
+const useFormReducer = ({ initialValues = {}, initialErrors = {},  initialSubminitAttempt = false, onReset, onSubmit, validationSchema = {}, initialValidation }) => {
+  const [state, dispatch] = useReducer(formReducer, {values: initialValues, errors: initialErrors, onReset, validationSchema }, initialization)
+  const helpers = {
+    setValues: (values) => dispatch({ type: 'SET_VALUES', payload: values }),
+    setErrors: (errors) => dispatch({ type: 'SET_ERRORS', payload: errors }),
+    setTouched: (touched) => dispatch({ type: 'SET_TOUCHED', payload: touched }),
+    setFieldValue: (name, value) => dispatch({ type: 'SET_FIELD_VALUE', payload: { name, value } }),
+    setFieldTouched: (name, touched) => dispatch({ type: 'SET_FIELD_TOUCHED', payload: { name, touched } }),
+    setFieldErrors: (name, error) => dispatch({ type: 'SET_FIELD_ERROR', payload: { name, error } }),
+    setIsValidating: (isValidating) => dispatch({ type: 'SET_ISVALIDATING', payload: { isValidating } }),
+    setIsSubmiting: (isSubmiting) => dispatch({ type: 'SET_ISSUBMITING', payload: { isSubmiting } }),
+    setSubmitAttempt: (submitAttempt) => dispatch({ type: 'SET_SUBMIT_ATTEMPT', payload: { submitAttempt } }),
+    setSubmitSuccess: (submitSuccess) => dispatch({ type: 'SET_SUBMIT_SUCCESS', payload: { submitSuccess } }),
+    setSubmitFailure: (submitFailure) => dispatch({ type: 'SET_SUBMIT_FAILURE', payload: { submitFailure } }),
+    reset: () => {
+      initialization()
+      onReset?.({values: initialValues, errors: initialErrors, onReset, validationSchema, onSubmit })
+    }
+  }
   return;
 };
+
+const FormProvider = ({initialValues, onReset, onSubmit, children}) => <FormContext.Provider>{childrend}</FormContext.Provider>;
+const FormConsumer = FormContext.Consumer;
+
+
+
+
+
 
 export default useFormContext;
 
